72dc3632f13aa745048745431d5ed3d650cbf5c0
mithlesh <mithleshnirmal123786@gmail.com>
2022-08-03 01:36:05 -0400

    udate

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,7 +3,7 @@ import img2 from '../Images/2.jpg';
 import img3 from '../Images/3.jpg';
 import img4 from '../Images/4.jpg';
 import img5 from '../Images/5.jpg';
-import img6 from '../Images/6.jpg';
+import img6 from '../Images/6.png';
 import img7 from '../Images/7.png';
 import img8 from '../Images/8.png';
 function Projects() {
@@ -16,35 +16,35 @@ function Projects() {
                     <div className="grid grid-flow-cols grid-cols-1 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-8 pt-10">
                         <div>
                             <img src={img1} alt="Dribble Desigen" className="bg-gray-200 py-1 px-1 img-box" />
-                            <a href="https://fervent-fermi-c83da0.netlify.app" className="text-xs text-gray-300 border-p">Clone of Tailblocks Desigen</a>
+                            <a href="https://fervent-fermi-c83da0.netlify.app" className="text-xs text-gray-300 border-p" target="_blank">Clone of Tailblocks Desigen</a>
                         </div>
                         <div>
                             <img src={img2} alt="Netflix-design" className="bg-gray-200 py-1 px-1 img-box" />
-                            <a href="https://pensive-turing-10a23d.netlify.app" className="text-xs text-gray-300 border-p">Clone of Netflix Desigen</a>
+                            <a href="https://pensive-turing-10a23d.netlify.app" className="text-xs text-gray-300 border-p" target="_blank">Clone of Netflix Desigen</a>
                         </div>
                         <div>
                             <img src={img3} alt="Discord-design" className="bg-gray-200 py-1 px-1 img-box" />
-                            <a href="https://frosty-rosalind-c7620e.netlify.app" className="text-xs text-gray-300 border-p">Clone of Discord Desigen</a>
+                            <a href="https://frosty-rosalind-c7620e.netlify.app" className="text-xs text-gray-300 border-p" target="_blank">Clone of Discord Desigen</a>
                         </div>
                         <div>
                             <img src={img4} alt="linkdin" className="bg-gray-200 py-1 px-1 img-box" />
-                            <a href="https://cranky-shockley-4144d7.netlify.app" className="text-xs text-gray-300 border-p">Clone of Linkedin Desigen</a>
+                            <a href="https://cranky-shockley-4144d7.netlify.app" className="text-xs text-gray-300 border-p" target="_blank">Clone of Linkedin Desigen</a>
                         </div>
                         <div>
                             <img src={img5} alt="Microsoft-design" className="bg-gray-200 py-1 px-1 img-box" />
-                            <a href="https://adoring-ptolemy-0c210a.netlify.app" className="text-xs text-gray-300 border-p">Clone of Microsoft Desigen</a>
+                            <a href="https://adoring-ptolemy-0c210a.netlify.app" className="text-xs text-gray-300 border-p" target="_blank">Clone of Microsoft Desigen</a>
                         </div>
                         <div>
-                            <img src={img6} alt="potfoliyo-mithlesh" className="bg-gray-200 py-1 px-1 img-box" />
-                            <a href="https://potfoliyo-mithlesh-by-html-css.netlify.app/" className="text-xs text-gray-300 border-p">First Potfoliyo</a>
+                            <img src={img6} alt="potfoliyo-mithlesh" className="bg-gray-200 py-1.5 px-1 img-box" />
+                            <a href="https://food-ordering-hub-mithlesh.netlify.app/" className="text-xs text-gray-300 border-p " target="_blank">First Potfoliyo</a>
                         </div>
                         <div>
                             <img src={img7} alt="" className="bg-gray-200 py-1 px-1 img-box" />
-                            <a href="https://foot-by-foot-project.netlify.app/" className="text-xs text-gray-300 border-p">Foot By Foot</a>
+                            <a href="https://foot-by-foot-project.netlify.app/" className="text-xs text-gray-300 border-p" target="_blank">Foot By Foot</a>
                         </div>
                         <div>
                             <img src={img8} alt="" className="bg-gray-200 py-1 px-1 img-box" />
-                            <a href="https://house-rental-website-by-mithlesh.netlify.app/" className="text-xs text-gray-300 border-p">Rentail website</a>
+                            <a href="https://house-rental-website-by-mithlesh.netlify.app/" className="text-xs text-gray-300 border-p" target="_blank">Rentail website</a>
                         </div>
                       
                     </div>
