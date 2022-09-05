d7108315a131e031427551077c203e6adaada2ba
Mithlesh Nirmal <mithleshnirmal786123@gmail.com>
2022-09-05 12:10:46 +0530

    first commit

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,3 +1,49 @@
+
+
+const Edudata = [{
+    Date: 2017,
+    Edu: "10th class",
+    School: "M.S.Memorial Model School, Jalandhar (Under : PSEB)"
+},
+{
+    Date: 2019,
+    Edu: "12th class",
+    School: "Govt. Model Ser Sec School P.A.P Campus (Under : PSEB)"
+},
+
+{
+    Date: 2022,
+    Edu: "BCA Course",
+    School: "Lyallpur Khalsa College Technical Campus, Punjab (Under : PTU)"
+},
+
+{
+    Date: 2021,
+    Edu: "HTML Course",
+    School: "GeeksforGeeks, Codecademy, LetsUpgrade, Great Learning"
+},
+{
+    Date: 2021,
+    Edu: "CSS Course",
+    School: "GeeksforGeeks, Codecademy, LetsUpgrade, EDUCBA"
+},
+{
+    Date: 2021,
+    Edu: "React JS(Beg.)",
+    School: "Linkedin Learning, Great Learning, Codecademy"
+},
+{
+    Date: 2022,
+    Edu: "Tailwind CSS",
+    School: "Linkedin Learning, Mindluster, Tailwind.css"
+},
+{
+    Date: 2022,
+    Edu: "Bootstrap(Beg.)",
+    School: "LetsUpgrade, Youtube, Bootstrap.com"
+},
+]
+
 function Education() {
     return (
         <div>
@@ -6,54 +52,21 @@ function Education() {
                     <h1 className="text-3xl font-bold text-gray-100 text-center mb-1"><span className="text-yellow-400">My</span> Education</h1>
                     <div className="w-full h-px bg-gray-800"></div>
                     <div className="grid grid-flow-cols grid-cols-1 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-8">
-                        <div className="box-achiv">
-                            <i className="fas fa-graduation-cap bg-yellow-400"></i>
-                            <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">2017</span>
-                            <h3 className="text-base font-bold py-2">10th Class</h3>
-                            <p className="text-xs">M.S.Memorial Model School, Jalandhar (Under : PSEB)</p>
-                        </div>
-                        <div className="box-achiv">
-                            <i className="fas fa-graduation-cap bg-yellow-400"></i>
-                            <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">2019</span>
-                            <h3 className="text-base font-bold py-2">12th Class</h3>
-                            <p className="text-xs">Govt. Model Ser Sec School P.A.P Campus (Under : PSEB)</p>
-                        </div>
-                        <div className="box-achiv">
-                            <i className="fas fa-graduation-cap bg-yellow-400"></i>
-                            <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">2022</span>
-                            <h3 className="text-base font-bold py-2">BCA Course</h3>
-                            <p className="text-xs">Lyallpur Khalsa College Technical Campus, Punjab (Under : PTU)</p>
-                        </div>
-                        <div className="box-achiv">
-                            <i className="fas fa-graduation-cap bg-yellow-400"></i>
-                            <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">2021</span>
-                            <h3 className="text-base font-bold py-2">HTML Course</h3>
-                            <p className="text-xs">GeeksforGeeks, Codecademy, LetsUpgrade, Great Learning</p>
-                        </div>
-                        <div className="box-achiv">
-                            <i className="fas fa-graduation-cap bg-yellow-400"></i>
-                            <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">2021</span>
-                            <h3 className="text-base font-bold py-2">CSS Course</h3>
-                            <p className="text-xs">GeeksforGeeks, Codecademy, LetsUpgrade, EDUCBA</p>
-                        </div>
-                        <div className="box-achiv">
-                            <i className="fas fa-graduation-cap bg-yellow-400"></i>
-                            <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">2022</span>
-                            <h3 className="text-base font-bold py-2">React JS(Beg.)</h3>
-                            <p className="text-xs">Linkedin Learning, Great Learning, Codecademy</p>
-                        </div>
-                        <div className="box-achiv">
-                            <i className="fas fa-graduation-cap bg-yellow-400"></i>
-                            <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">2022</span>
-                            <h3 className="text-base font-bold py-2">Tailwind CSS</h3>
-                            <p className="text-xs">Linkedin Learning, Mindluster, Tailwind.css</p>
-                        </div>
-                        <div className="box-achiv">
-                            <i className="fas fa-graduation-cap bg-yellow-400"></i>
-                            <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">2022</span>
-                            <h3 className="text-base font-bold py-2">Bootstrap(Beg.)</h3>
-                            <p className="text-xs">LetsUpgrade, Youtube, Bootstrap.com</p>
-                        </div>
+
+                        {
+                            Edudata.map((val, ind) => {
+                                return (
+                                    <div className="box-achiv">
+                                        <i className="fas fa-graduation-cap bg-yellow-400"></i>
+                                        <span className="text-xs font-bold text-gray-400 py-1 px-4 bg-gray-800 rounded-full hover:bg-yellow-400">{val.Date}</span>
+                                        <h3 className="text-base font-bold py-2">{val.Edu}</h3>
+                                        <p className="text-xs">{val.School}</p>
+                                    </div>
+                                )
+                            })
+                        }
+
+
                     </div>
                 </div>
             </section>
